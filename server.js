fcd5f4e4f8375aa4621f6661c187ee3a2f8966bb
Kevin Gomes <kevingomes17@gmail.com>
2015-04-30 19:06:54 -0500

    Changing port as per deployment requirements on heroku

--- server.js
+++ server.js
@@ -94,7 +94,9 @@ app.use(function(err, req, res, next) {
 
 //module.exports = app;
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+console.log(process.env);
+var server_port = process.env.PORT || 3000;
+//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
 app.listen(server_port, server_ip_address, function () {
